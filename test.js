aeb3db8691c2756c47ceba7acc886f314a9cdc62
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-30 16:08:46 -0600

    Added test

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('create a greeting', ()=> {
+  expect(getGreeting('Joe')).toBe("Hi Joe")
+})
